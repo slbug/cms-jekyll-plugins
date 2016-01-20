061510c3e3665d8f044916779ea6d5183546a8c5
Alexander Grebennik <aleks.grebennik@gmail.com>
2016-01-20 17:20:34 +0300

    convert hashes to arrays

--- i18n_data.rb
+++ i18n_data.rb
@@ -9,7 +9,25 @@ module Jekyll
       @locale = @site.active_lang
       @default_locale = @site.default_lang
 
-      @content = assign_associations(translate_data(@content, @locale, @default_locale))
+      @content = assign_associations(translate_data(@content, @locale, @default_locale)).tap do |content|
+        content['_models'] = convert_models(content['_models'])
+      end
+    end
+
+    def convert_models(models)
+      data_pages = @site.config['page_gen'].map{|p| p['data'] }
+
+      models.each do |model, values|
+        if values.kind_of?(Hash) && !data_pages.include?(model)
+          models[model] = [].tap do |content|
+            values.keys.map(&:to_i).sort.each do |key|
+              content << values[key.to_s].tap{|val| val['id'] = key }
+            end
+          end
+        end
+      end
+
+      models
     end
   end
 end
